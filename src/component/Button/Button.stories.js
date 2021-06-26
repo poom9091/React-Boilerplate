e25045aab3c208e8353daabc1510e94f0f7c6a65
Pubadee Klinocha <poom90914@gmail.com>
2021-06-27 03:05:08 +0700

    Story Book

---
+++ src/component/Button/Button.stories.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import Button from './Button'
+import Center from '../Center/Center'
+export default{
+    title: 'Form/Button',
+    component: Button,
+    decorators: [story => <Center> {story()}</Center>],
+    args:{
+        children: 'Button',
+        onClick: { action: 'clicked'}
+    }
+}
+
+export const Primary = () => <Button variant='primary'> Primary </Button>
+export const Secondary = () => <Button variant='secondary'> Secondary </Button>
+export const Success  = () => <Button variant='success'> Success </Button>
+export const Danger = () => <Button variant='danger'> Danger </Button>
+export const log = () => <Button variant='primary' onClick={() => console.log("Button click")}>Log</Button>
+
+
+const Template = args => <Button {...args}></Button>
+
+export const PrimaryA = Template.bind({})
+PrimaryA.args = {
+    variant: 'primary',
+    // children: 'Primary Args'
+}
+
+export const LongPrimaryA = Template.bind({})
+LongPrimaryA.args={
+    ...PrimaryA.args,
+    // children: 'Long Primary Ags'
+}
+
