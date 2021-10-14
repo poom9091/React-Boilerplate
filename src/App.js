0c2e01e996ee19a1e06c270e27c4c359eaea020b
Pubadee Klinocha <poom90914@gmail.com>
2021-10-14 13:01:33 +0700

    Add taliwind

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import 'tailwindcss/tailwind.css'
 import Boilerplate from "./component/Boilerplate/Boilerplate";
 import React from "react";
 
 function App() {
   return (
     <div className="App">
+      
       <Boilerplate/>
     </div>
 
--- src/component/Boilerplate/Boilerplate.jsx
+++ src/component/Boilerplate/Boilerplate.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import Testgraphql from "../Graphql/Testgraphql";
 import Buttontest from "../Buttontest/Buttontest";
 import RestAPI from "../RestAPI/RestAPI";
+import 'tailwindcss/tailwind.css'
 import './Boilerplate.css'
+
 function Boilerplate() {
     return (
         <div class="example">
-            <h1 class="header-1">Hi this is Boilderplate React</h1>
+            <h1 class=" text-gray-700 font-semibold">Boilderplate React</h1>
             <div class="centent-plate">
                 <div class='flex-inline text-jest'>
                     <h1>Test Jest</h1>
